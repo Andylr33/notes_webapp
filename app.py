e3069ad8be77818677d23ef2d06086ee0ca56203
Andylr33 <andres.ramos03@utrgv.edu>
2023-12-17 18:09:56 -0600

    Added security methods such as login_required and session to keep track of users logged in

--- app.py
+++ app.py
@@ -1,35 +1,79 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, session
 import os
-# from flask_login import UserMixin
-# from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from database import create_user_account, add_note, check_if_user_exists, get_user_info
+from flask_login import login_user, login_required, logout_user, current_user
+from flask_session import Session
+from helpers import login_required
 
 app = Flask(__name__)
 
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/")
+@login_required
 def home():
     return render_template("home.html")
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    # Forget any user_id
+    session.clear()
+
     if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
         data = request.form
-        print(data)
+        user = get_user_info(data) # dictionary of users information/credentials
+        if check_if_user_exists(data):
+            if check_password_hash(user['hash'], password):
+                flash("Logged in successfully!", category="success")
+                
+                # remember which user has logged in
+                session["user_id"] = user['id']
+                # set logged in to True
+                session['logged_in'] = True
+
+                return redirect("/")
+            else:
+                flash("Incorrect password, try again.", category="danger")
+        else:
+            flash("Email does not exist.", category="danger")
     return render_template("login.html")
 
 @app.route("/logout")
+@login_required
 def logout():
-    return "<p> Logout <p>"
+    # Forget any user_id
+    session.clear()
+    session['logged_in'] = False
+    
+    return redirect("/")
 
 @app.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
+    # forget any user id
+    session.clear()
+
     if request.method == 'POST':
         email = request.form.get('email')
-        full_name = request.form.get('fullName')
+        full_name = request.form.get('full_name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+        data = request.form # dictionary that contains data from sign up page
 
         # verify information
-        if len(email) < 4:
+        if check_if_user_exists(data):
+            flash("An account with that email already exists!", category='danger')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='danger')
         elif len(full_name) < 4:
             flash('Full name must be greater than 3 characters.', category='danger')
@@ -39,7 +83,16 @@ def sign_up():
             flash('Password must be at least 7 characters.', category='danger')
         else:
             # add user to database
+            
+            create_user_account(data)
             flash('Your account was created!', category='success')
+
+            user = get_user_info(data)
+            session["user_id"] = user['id']
+            # set logged in to True
+            session['logged_in'] = True
+
+            return redirect("/")
     return render_template("sign_up.html")
 
 # Configure session to use filesystem (instead of signed cookies)
@@ -48,9 +101,9 @@ def sign_up():
 app.secret_key = os.environ.get('SECRET_KEY')
 
 # Configure session to use filesystem (instead of signed cookies)
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,10 +1,45 @@
 from sqlalchemy import create_engine, text
 from flask_login import UserMixin
 import os
+from werkzeug.security import check_password_hash, generate_password_hash
 
 db_connection_string = os.environ['NOTES_APP_DB']
 engine = create_engine(db_connection_string) # "mysql+pymysql://sylvain:passwd@localhost/db?host=localhost?port=3306" <-- sample of how to connect to local machine
 
-with engine.connect() as conn:
-    print("Database was connected!")
+def create_user_account(data):
+    with engine.connect() as conn:
+        query = "INSERT INTO users (email, hash, full_name) VALUES (:email, :hash, :full_name);"
+        
+        conn.execute(text(query), 
+                        {"email": data['email'], "hash": generate_password_hash(data['password1']),
+                        "full_name": data['full_name']})
+        conn.execute(text("commit;"))                   # MUST COMMIT TO SEE CHANGES IN DATABASE!!
+        print("User was added to the database!")
 
+def check_if_user_exists(data):
+    with engine.connect() as conn:
+        query = "SELECT * FROM users WHERE email = :email;"
+        result = conn.execute(text(query),
+                    {"email": data['email']})
+        rows = result.all()
+        # print(f"The length of rows is {len(rows)}")
+        if len(rows) ==  1:
+            return True
+        else:
+            return False
+        
+def get_user_info(data):
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT * FROM users WHERE email = :email;"), {"email": data['email']}) # pass in parameters as dictionary key-value pair
+            rows = result.all()
+            if len(rows) == 0:
+                return None
+            else: 
+                return dict(rows[0]._mapping)
+
+
+def add_note(user_id):
+    pass
+
+def display_notes():
+    pass
